3f024f1749641ed643c48912c775ec80262ce977
Avi1718 <58083389+Avi-Rana@users.noreply.github.com>
2020-06-25 00:40:52 +0530

    Index.js1

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {	
+console.log('BOT IS ONLINE!');
+client.user.setActivity('with a working status!');
+});
+
+client.on('message' , message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if (command === 'status') {
+        client.commands.get('status').execute(client,message, args);
+    } else if (command === 'serverinfo') {
+        client.commands.get('serverinfo').execute(message, args);
+    }
+});
+
+        
+client.login(token);
