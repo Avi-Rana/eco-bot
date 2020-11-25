e4c1dec3019344bc32d778b0baa0a31c7658e618
Avi1718 <58083389+Avi-Rana@users.noreply.github.com>
2020-11-25 22:44:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-  }
-})
-client.login("NzQzMzU0ODUwMDQ5MzkyNzIx.XzTdJw.Tjgg1VRGGpoa-qGqrtJtURdeu0k")
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
